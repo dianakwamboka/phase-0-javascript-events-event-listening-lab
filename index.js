05b3c9fdaa586ed3868ea6b0eadcf2bd706cdf61
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-28 17:06:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+// function addingEventListener() {
+//     const input = document.getElementById('input')
 
-function clickAlert() {
-  alert('I was clicked!');
-}
+// function clickAlert() {
+//   alert('I was clicked!');
 
-input.addEventListener('click', clickAlert); 
+// }
+
+// input.addEventListener('click', clickAlert); 
   
+// }
+//  function addEventListener() {
+// document.getElementById("#button").addingEventListener
+// ("click",alertMe)
+// function alertMe(){
+// return alert(" I was clicked!")
+// }
+//  }
+// function addEventListener(){
+//   console.log(document.getElementsById("input"))
+// }
+// document.querySelector("#input").addEventListener("click",() => alert("Hi I was clicked"))
+
+function addingEventListener() {
+  alert('I was clicked');
+  const input = document.getElementById("button");
+  input.addEventListener('click', addingEventListener);
 }
